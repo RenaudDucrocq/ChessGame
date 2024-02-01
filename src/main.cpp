cd8056e64e456d27a790103bf2076c02cd1693e6
Pierre Dedrie <Pirgosth74@gmail.com>
2024-02-01 11:23:19 +0100

    feat: added SFML to project (#3)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "Coordonnees.hpp"
 #include "Pieces/Bishop.hpp"
-int main() {
+
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
     Coordonnees c = Coordonnees(1, 'A');
     std::cout << c.to_String();
@@ -13,6 +16,20 @@ int main() {
     auto *b = new Bishop(c, *p);
 
     std::cout << b->getValue();
+
+    sf::Window window(sf::VideoMode(800, 600), "ChessGame");
+
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 
     return 0;
 }
