341a7af01cfb9396dac45dea0335d7891264b7a9
Pirgosth <pirgosth74@gmail.com>
2024-02-03 15:34:24 +0100

    feat: added vscode launch and debug tasks

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char* argv[])
 {
     std::cout << "Hello World from Client !" << std::endl;
+    
+    sf::RenderWindow window(sf::VideoMode(800, 600), "ChessGame");
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color(255, 255, 255));
+        window.display();
+    }
+
     return 0;
 }
